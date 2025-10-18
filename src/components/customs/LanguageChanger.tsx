baf2d29a50bfa1f6b957002a6db4ff1b34b25acf
Mayeul DERIES <mayeul.deries@etu.univ-grenoble-alpes.fr>
2025-10-18 11:06:42 +0200

    change button to hyperlink

--- src/components/customs/LanguageChanger.tsx
+++ src/components/customs/LanguageChanger.tsx
@@ -2,7 +2,6 @@ import { useTranslation } from "react-i18next";
 import { useState } from "react";
 import { FR, US } from "country-flag-icons/react/3x2";
 import { toast } from "sonner";
-import { Button } from "../ui/button";
 
 export const LanguageChanger = () => {
   const {
@@ -21,17 +20,21 @@ export const LanguageChanger = () => {
   };
 
   return (
-    <Button variant="ghost" size="sm" onClick={handleChangeLanguage}>
+    <a
+      onClick={(e) => {
+        e.preventDefault();
+        handleChangeLanguage();
+      }}
+      className="relative inline-flex items-center justify-center h-4 w-8 cursor-pointer select-none"
+    >
       <FR
-        className={`h-[1.2rem] w-[1.2rem] transition-all duration-300 ${
-          currentLanguage === "fr" ? "rotate-0 scale-100" : "rotate-90 scale-0"
-        }`}
+        className={`h-[1rem] w-[1rem] transition-all duration-300 ${currentLanguage === "fr" ? "rotate-0 scale-100" : "rotate-90 scale-0"}`}
       />
       <US
-        className={`absolute h-[1.2rem] w-[1.2rem] transition-all duration-300 ${
+        className={`absolute h-[1rem] w-[1rem] transition-all duration-300 ${
           currentLanguage === "en" ? "rotate-0 scale-100" : "rotate-90 scale-0"
         }`}
       />
-    </Button>
+    </a>
   );
 };
--- src/components/customs/ThemeChanger.tsx
+++ src/components/customs/ThemeChanger.tsx
@@ -1,13 +1,15 @@
-import { Button } from "../ui/button";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "@/providers/ThemeProvider";
 
 export const ThemeChanger = () => {
   const { setTheme, theme } = useTheme();
   return (
-    <Button variant="ghost" className="cursor-pointer" size="sm" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-    </Button>
+    <a
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      className="relative flex items-center justify-center cursor-pointer select-none rounded-sm p-2 transition-colors hover:bg-transparent focus:outline-none"
+    >
+      <Sun className="h-[1rem] w-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1rem] w-[1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    </a>
   );
 };
