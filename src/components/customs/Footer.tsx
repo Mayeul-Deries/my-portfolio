733bb7985b70cfd424bcd2f9ff46eb4fd5fd0500
Mayeul DERIES <mayeul.deries@etu.univ-grenoble-alpes.fr>
2025-10-20 16:41:19 +0200

    feat: update footer

--- src/components/customs/Footer.tsx
+++ src/components/customs/Footer.tsx
@@ -8,8 +8,18 @@ export const Footer = () => {
 
   return (
     <footer className="bg-white dark:bg-black border-t border-gray-200 dark:border-neutral-800">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 py-6">
-        <div className="flex flex-col md:flex-row justify-between items-center">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 pt-6 pb-8">
+        <div className="flex flex-col items-center space-y-3">
+          <div className="text-xl font-bold text-black dark:text-white">Mayeul Deries</div>
+          <p className="text-center text-xs sm:text-sm text-muted-foreground caption max-w-lg">
+            {t("footer.rotating_texts.fullstack_dev")} | {t("footer.rotating_texts.tech_explorer")} |{" "}
+            {t("footer.rotating_texts.uiux_lover")}
+          </p>
+          <span className="w-full text-center sm:w-auto text-sm sm:text-sm text-gray-600 dark:text-neutral-300">
+            {t("footer.built_with")}
+          </span>
+        </div>
+        <div className="flex flex-col md:flex-row pt-2 justify-between items-center">
           <p className="text-sm text-gray-500 dark:text-neutral-400">
             © {currentYear} {t("footer.rights")}
           </p>
